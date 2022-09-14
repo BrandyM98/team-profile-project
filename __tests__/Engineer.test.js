321e4328ed1c6e2067c3e185fd68b210478139d2
Brandy Moore <brandymoore923@gmail.com>
2022-09-13 21:46:13 -0400

    added engineer test and passed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,5 +1,20 @@
 const Engineer = require("../lib/Engineer");
 
-describe("Engineer", () => {
+test("creates engineer obj", () => {
+    const engineer = new Engineer('China', 12345, 'china@gmail.com', "BrandyM98");
+    expect(engineer.github) .toEqual(expect.any(String));
+});
 
+//gets github
+test('gets github', () => {
+    const engineer = new Engineer('China', 12345, 'china@gmail.com', "BrandyM98");
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+
+});
+
+
+//gets role
+test('gets role', () => {
+    const engineer = new Engineer('China', 12345, 'china@gmail.com', "BrandyM98");
+    expect(engineer.getRole()).toEqual("Engineer");
 });
