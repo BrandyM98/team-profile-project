17fa07752b36d05c299532c11060abc860c46f8b
Brandy Moore <brandymoore923@gmail.com>
2022-09-13 21:36:37 -0400

    added employee test. one failed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,34 @@
 //this would be the employee constructor
 const Employee = require("../lib/Employee");
 
-test("Employee", () => {
-    const employee = new Employee ();
+test("creates Employee obj", () => {
+    const employee = new Employee('China', 12345, 'china@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
 
+//gets name
+test('gets name', () => {
+    const employee = new Employee('China', 12345, 'china@gmail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+//gets id
+test('gets id', () => {
+    const employee = new Employee('China', 12345, 'china@gmail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+//gets email
+test('gets email', () => {
+    const employee = new Employee('China', 12345, 'china@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+//gets role
+test('gets role', () => {
+    const employee = new Employee('China', 12345, 'china@gmail.com');
+    expect(employee.getRole()).toEqual("Employee");
+});
