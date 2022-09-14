42bebf37ffb245534bb3aecaaa464423eb7b1ef7
Brandy Moore <brandymoore923@gmail.com>
2022-09-13 21:50:05 -0400

    added intern test and passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,5 +1,19 @@
 const Intern = require("../lib/Intern");
 
-describe("Intern", () => {
+test("creates intern obj", () => {
+    const intern = new Intern('China', 12345, 'china@gmail.com', "CBU");
+    expect(intern.school) .toEqual(expect.any(String));
+});
 
+//gets school
+test('gets school', () => {
+    const intern = new Intern('China', 12345, 'china@gmail.com', "CBU");
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+
+});
+
+//gets role
+test('gets role', () => {
+    const intern = new Intern('China', 12345, 'china@gmail.com', "CBU");
+    expect(intern.getRole()).toEqual("Intern");
 });
