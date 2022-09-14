28de0560af5180d7496dd1246afb98b78d9b64f7
Brandy Moore <brandymoore923@gmail.com>
2022-09-13 21:59:22 -0400

    added manager test. passed test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,12 @@
 const Manager = require("../lib/Manager");
 
-describe("Manager", () => {
-
+test("creates manager obj", () => {
+    const manager = new Manager('China', 12345, 'china@gmail.com', 6);
+    expect(manager.officeNumber) .toEqual(expect.any(Number));
 });
 
+//gets role
+test('gets role', () => {
+    const manager = new Manager('China', 12345, 'china@gmail.com');
+    expect(manager.getRole()).toEqual("Manager");
+});
